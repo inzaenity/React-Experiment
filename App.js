4b46edfd4d82c6aba9d99b5553fb180df8e135c6
inzaenity <118352275+inzaenity@users.noreply.github.com>
2023-02-12 13:49:48 +1100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import NavBar from "./components/Navbar.js"
+import Main from "./components/Main.js"
+
+function App() {
+    return(
+        <div className="container">
+            <NavBar />
+            <Main />
+        </div>
+    )
+}
+
+export default App
